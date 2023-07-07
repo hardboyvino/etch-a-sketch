eb40d9633dab930e2c2b1ad05561b52b384b0c14
Babaneyo <97181625+hardboyvino@users.noreply.github.com>
2023-07-07 13:19:02 +0100

    Create the initial website interface
    The basic interface has the buttons and the only the new sketch button works to create a new grid

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 function makeGrid(widthSquares, heightSquares) {
     let container = document.getElementById("grid-container");
 
+    container.style.gridTemplateColumns = `repeat(${widthSquares}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${heightSquares}, 1fr)`;
+
     for (let i = 0; i < (widthSquares * heightSquares); i++) {
         let square = document.createElement("div");
         square.classList.add("square");
         container.appendChild(square);
+    };
+};
+
+
+function startSketch() {
+    let userPrompt = parseInt(prompt("What size of grid do you want? ", 16));
+
+    if (Number.isInteger(userPrompt) === true && userPrompt > 0 && userPrompt < 65) {
+        alert(`You have chosen a ${userPrompt} by ${userPrompt} grid to sketch`);
+        makeGrid(userPrompt, userPrompt);
+    } else {
+        alert("Enter a number between 1 and 64 (inclusive)");
     }
 }
 
-makeGrid(16, 16);
+
+// makeGrid(16, 16);
