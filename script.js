533a9f5260bec7ac36d73c9f8d067d635044afd8
Babaneyo <97181625+hardboyvino@users.noreply.github.com>
2023-07-07 14:16:27 +0100

    Make the grid change colour when clicked

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-function makeGrid(widthSquares, heightSquares) {
-    let container = document.getElementById("grid-container");
+let container = document.getElementById("grid-container");
+let clickedColour = "black";
 
+function makeGrid(widthSquares, heightSquares) {
     container.style.gridTemplateColumns = `repeat(${widthSquares}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${heightSquares}, 1fr)`;
 
@@ -13,6 +14,8 @@ function makeGrid(widthSquares, heightSquares) {
 
 
 function drawGrid() {
+    container.innerHTML = "";
+    
     let userPrompt = parseInt(prompt("What size of grid do you want? ", 16));
 
     if (Number.isInteger(userPrompt) === true && userPrompt > 0 && userPrompt < 65) {
@@ -22,7 +25,17 @@ function drawGrid() {
         alert("Enter a number between 1 and 64 (inclusive). Defaulting to a 16x16 grid.");
         makeGrid(16, 16);
     }
+    makeGridInteractive(clickedColour);
 }
 
+function makeGridInteractive(clickedColour) {
+    // let squares = document.querySelectorAll(".square");
 
-// makeGrid(16, 16);
+    document.querySelectorAll(".square").forEach(square => {
+        square.addEventListener("click", () => {
+            square.style.backgroundColor = clickedColour;
+        });
+    });
+}
+
+drawGrid();
